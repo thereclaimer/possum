06a805f7ceb66472391b80acf4103427ae0f0c9c
thereclaimer <sam.davenport@thereclaimergame.com>
2024-05-26 15:08:32 -0500

    adding mat4

---
+++ src/library/possum-math-mat4.hpp
@@ -0,0 +1,59 @@
+#ifndef POSSUM_MATH_MAT4_HPP
+#define POSSUM_MATH_MAT4_HPP
+
+#include <math.h>
+
+#include "possum-types.hpp"
+#include "possum-math-simd-4f32.hpp"
+
+struct PossumMathMat4Row {
+    f32 col0; 
+    f32 col1; 
+    f32 col2; 
+    f32 col3; 
+};
+
+struct PossumMathMat4 {
+
+    union {
+
+        struct {
+
+            f32 row0_col0;
+            f32 row0_col1;
+            f32 row0_col2;
+            f32 row0_col3;
+
+            f32 row1_col0;
+            f32 row1_col1;
+            f32 row1_col2;
+            f32 row1_col3;
+            
+            f32 row2_col0;
+            f32 row2_col1;
+            f32 row2_col2;
+            f32 row2_col3;
+
+            f32 row3_col0;
+            f32 row3_col1;
+            f32 row3_col2;
+            f32 row3_col3;
+        };
+
+        union {
+
+            struct {
+                PossumMathMat4Row row0;
+                PossumMathMat4Row row1;
+                PossumMathMat4Row row2;
+                PossumMathMat4Row row3;
+            };
+
+            PossumMathMat4Row rows[4];
+        };
+
+        f32 values[16];
+    };
+};
+
+#endif //POSSUM_MATH_MAT4_HPP
--- src/library/possum-math.hpp
+++ src/library/possum-math.hpp
@@ -5,6 +5,7 @@
 #include "possum-math-simd.hpp"
 #include "possum-math-vec2.hpp"
 #include "possum-math-vec3.hpp"
+#include "possum-math-mat4.hpp"
 
 #define POSSUM_MATH_SIZE_KILOBYTE         1024
 #define POSSUM_MATH_SIZE_KILOBYTES(size)  (size * POSSUM_MATH_SIZE_KILOBYTE);
@@ -13,9 +14,12 @@
 
 typedef PossumMathVec2 vec2f32;
 typedef PossumMathVec3 vec3f32;
+typedef PossumMathMat4 mat4f32;
 
 typedef vec2f32* vec2f32ptr;
 typedef vec3f32* vec3f32ptr;
+typedef mat4f32* mat4f32ptr;
+
 
 inline void
 possum_math_f32_add_batch(
