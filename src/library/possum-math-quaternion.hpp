2f61d58fa28467c1d3b7d3062022cded91b62dc9
Sam Davenport <sam@davenportdronographics.com>
2024-05-29 20:52:39 -0500

    working on adding quaternions

---
+++ src/library/possum-math-quaternion.hpp
@@ -0,0 +1,13 @@
+#ifndef POSSUM_MATH_QUATERNION_HPP
+#define POSSUM_MATH_QUATERNION_HPP
+
+#include "possum-types.hpp"
+
+struct PossumMathQuaternion {
+    f32 x;
+    f32 y;
+    f32 z;
+    f32 w;
+};
+
+#endif //POSSUM_MATH_QUATERNION_HPP
